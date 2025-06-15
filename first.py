c21a8870d615e22da0029af6dc774b757c40bcfc
keshav-v2004 <keshavcse.ucs1536@gmail.com>
2025-06-15 13:18:08 +0530

    get the total number of rows,iterate over them and get values from required columns

--- first.py
+++ first.py
@@ -5,3 +5,13 @@ sheet = wb['Sheet1'] # load the first sheet
 
 cell = sheet["A1"] # value of cell gets loaded
 print(cell.value)
+print()
+
+print(sheet.max_row) ## returns the maximum number of rows in the sheet (also includes the header i.e row names)
+
+print()
+for eachRow in range(2,sheet.max_row+1):
+
+    print(sheet.cell(eachRow,3).value)
+    # we want third column (index=3) of every row
+
